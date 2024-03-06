84300d0a0d510cec4b82dd50f6894d0f149f9cf0
yomna <yumnamohamed30@gmail.com>
2024-03-06 02:58:16 +0200

    editing

--- db/initconnection.js
+++ db/initconnection.js
@@ -1,11 +1,28 @@
 
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const initconnection =()=>{
-// mongoose.connect('mongodb://127.0.0.1:27017/ProjectDb')
-mongoose.connect("mongodb+srv://Yomna-mohamed:ghxuO3Pa4OkKEE1H@cluster0.zbpwgph.mongodb.net/E-commerce")
-.then(()=>console.log("DB connected"))
-.catch((err)=>console.log("error", err))
+// const initconnection =()=>{
+// // mongoose.connect('mongodb://127.0.0.1:27017/ProjectDb')
+// mongoose.connect("mongodb+srv://Yomna-mohamed:ghxuO3Pa4OkKEE1H@cluster0.zbpwgph.mongodb.net/E-commerce")
+// .then(()=>console.log("DB connected"))
+// .catch((err)=>console.log("error", err))
+// }
+
+// export default initconnection;
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
+
+const initconnection = () => {
+    const mongodbURI = process.env.MONGODB_URI; // Retrieve MongoDB connection string from environment variable
+
+    return mongoose.connect(mongodbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log("Connected to MongoDB"))
+        .catch((err) => console.error("Error connecting to MongoDB:", err));
 }
 
 export default initconnection;
